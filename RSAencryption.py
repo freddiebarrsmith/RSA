a94a665b266150c0b6960ef2dea45bbbbfbb0cc5
root <root@localhost.localdomain>
2016-01-30 18:38:08 +0000

    2nd

--- RSAencryption.py
+++ RSAencryption.py
@@ -4,6 +4,9 @@ p = 3
 q = 11
 m = 2
 
+def plaintextencoder(m):
+    return None
+
 def ncalculate(p, q):
         n = p * q
         return n
@@ -31,7 +34,33 @@ def exponentcalculator(phi):
 #e = exponentcalculator(phi)
 e = 7
 
-d = 3
+#d = 3
+def multiplicative_inverse(e, phi):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_phi = phi
+
+    while e > 0:
+        temp1 = temp_phi/e
+        temp2 = temp_phi - temp1 * e
+        temp_phi = e
+        e = temp2
+
+        x = x2- temp1* x1
+        y = d - temp1 * y1
+
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+
+    if temp_phi == 1:
+        return d
+d =  multiplicative_inverse(e, phi)
+print "d:"
+print d
 
 def encryptionalgo(m, d, e, phi):
     print m
@@ -39,7 +68,7 @@ def encryptionalgo(m, d, e, phi):
     print e
     cipher1 = math.pow(m, e)
     cipher2 = cipher1 % n
-    print "encrypted text"
+    print "ciphertext"
     print cipher2
     return cipher2
 
